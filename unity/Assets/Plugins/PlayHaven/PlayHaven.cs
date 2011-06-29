11e400973707b848360d5a3b5b86d864b04225e7
Jesus Fernandez <jesus@playhaven.com>
2011-06-29 09:14:23 -0700

    looks like there's not a way to automate XCode integration through Applescript, yikes

---
+++ unity/Assets/Plugins/PlayHaven/PlayHaven.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayHaven : MonoBehaviour {
+	protected static Hashtable sRequests;
+	// Use this for initialization
+	void Start () {
+		sRequests = new Hashtable();
+	}
+	
+	public static void Open(string token, string secret){
+		
+	}
+	
+}
